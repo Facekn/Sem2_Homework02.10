20322033eefa3d4e0ff31a4ab21cc40e69d9493e
Ilya Grigorev <facekn110388@gmail.com>
2022-10-08 22:43:29 +0300

    Диапазон координат для заданной четверти

---
+++ Sem2_Homework3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите номер четверти для плоскости: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number==1)
+{
+    Console.WriteLine("Диапазон возможных координат xy: x>0, y>0");
+} 
+
+else if (number==2)
+{
+    Console.WriteLine("Диапазон возможных координат xy: x<0, y>0");
+} 
+
+else if (number==3)
+{
+    Console.WriteLine("Диапазон возможных координат xy: x<0, y<0");
+} 
+
+else if (number==4)
+{
+    Console.WriteLine("Диапазон возможных координат xy: x>0, y<0");
+} 
+
+ 
+else 
+{
+    Console.WriteLine("Не является четвертью. Попробуйте еще раз!");
+}
