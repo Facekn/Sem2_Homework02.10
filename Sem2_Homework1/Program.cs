ba46a9779e533104c8f3bb8184833b5505a54aa5
Ilya Grigorev <facekn110388@gmail.com>
2022-10-08 22:05:30 +0300

    Вычислить какой день выходной

---
+++ Sem2_Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели: ");
+int daynumber = int.Parse(Console.ReadLine() ?? "0");
+
+if (daynumber > 0 && daynumber <= 5)
+{
+    Console.Write("К сожалению, это не выходной день, а рабочий");
+}
+else if (daynumber >= 6 && daynumber <= 7)
+
+{
+   Console.Write("Это выходной день"); 
+}
+
+else Console.Write("Дня недели с таким номер не существует!!!");
